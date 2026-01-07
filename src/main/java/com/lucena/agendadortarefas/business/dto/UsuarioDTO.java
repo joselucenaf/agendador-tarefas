de8705074329b1a5d452b9a8a943acd7df49ead7
Jose Lucena <joselucenafn@gmail.com>
2026-01-07 15:49:32 -0300

    Adicionadas configs de segurança

---
+++ src/main/java/com/lucena/agendadortarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,14 @@
+package com.lucena.agendadortarefas.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+}

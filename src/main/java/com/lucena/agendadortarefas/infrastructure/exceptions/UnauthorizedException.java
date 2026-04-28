5fac43d35e20cd1f79a80f14dec6b1487a276bfb
joselucenaf <joselucenafn@gmail.com>
2026-04-27 22:56:46 -0300

    Configurando o GitIgnore

---
+++ src/main/java/com/lucena/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.lucena.bffagendadortarefas.infrastructure.exceptions;
+
+public class UnauthorizedException extends RuntimeException  {
+    public UnauthorizedException(String mensagem) {
+        super(mensagem);
+    }
+    public UnauthorizedException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
